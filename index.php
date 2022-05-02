167c9d36a808fb3c5bdcda1f31077f8dd2311d64
MarcelBienko3b <m.bienko.3b19@gmail.com>
2022-05-02 14:16:44 +0200

    Basic styling for better data readibility on index

--- index.php
+++ index.php
@@ -5,20 +5,27 @@
 
         <meta charset="UTF-8">
         <title>ZSTI Together</title>
+
+        <link rel="stylesheet" href="styles/main.css">
         
     </head>
 
     <body>
 
-        <a href="register.php">Zarejestruj się</a>
-        <a href="loginStudent.php">Zaloguj się jako uczeń</a>
-        <a href="loginTeacher.php">Zaloguj się jako nauczyciel</a>
+        <nav class="navbar navbar--horizontal">
+            <ul class="navbar__list">
+                <li class="navbar__item"><a href="register.php">Zarejestruj się</a></li>
+                <li class="navbar__item"><a href="loginStudent.php">Zaloguj się jako uczeń</a></li>
+                <li class="navbar__item"><a href="loginTeacher.php">Zaloguj się jako nauczyciel</a></li>
+            </ul>
+        </nav>
 
         <?php
         
             $conn = new mysqli('localhost','root','','zsti_together_database');
 
-            $postsQuery = 'select 
+            $postsQuery = '
+                    select 
                         posts.post__body,
                         subjects.subject__name,
                         students.student__firstName,
@@ -32,31 +39,35 @@
                     left join students on students.student__id = posts.post__tutor
                     left join teachers on teachers.teacher__id = posts.post__teacher
                     inner join types on types.type__id = posts.post__type
-                    left join classes on classes.class__id = students.student__class';
+                    left join classes on classes.class__id = students.student__class
+                    ';
             
             $result = $conn->query($postsQuery);
 
             echo '<br>';
 
             if ($result->num_rows > 0) {
+                echo '<div class="posts-container">';
                 while ($rows = $result->fetch_all()) {
                     foreach ($rows as $row) {
                         echo 
-                            '<div class="post">
-
-                                <h3 class="description">'.$row[0].'</h3>
-                                <h4 class="subject">'.$row[1].'</h2>';
+                            '<div class="posts-container__post post">';
                                 if ($row[4] == null && $row[5] == null) {
-                                    echo '<h4 class="tutor">'.$row[2].' '.$row[3].' | '.$row[7].'</h2>';
+                                    echo '<h4 class="post__author">'.$row[2].' '.$row[3].' | '.$row[7].'</h2>';
                                 } elseif ($row[2] == null && $row[3] == null) {
-                                    echo '<h4 class="teacher">'.$row[4].' '.$row[5].'</h2>';
+                                    echo '<h4 class="post__author">'.$row[4].' '.$row[5].'</h2>';
                                 }
                             echo
-                                '<p class="type">'.$row[6].'</p>
+                                '<h3 class="post__description">'.$row[0].'</h3>
+                                <h4 class="post__subject">'.$row[1].'</h2>';
+                                
+                            echo
+                                '<p class="post__type">'.$row[6].'</p>
                             </div>';
                         echo '<br>';
                     }
                 }
+                echo '</div>';
             }
 
         ?>
