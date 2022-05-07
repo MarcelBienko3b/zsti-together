6da4d03d163ea268592f8ef29a1319251470a611
MarcelBienko3b <m.bienko.3b19@gmail.com>
2022-05-07 13:51:46 +0200

    Fixed nav buttons function

--- scripts/php/snippets/nav.php
+++ scripts/php/snippets/nav.php
@@ -11,33 +11,50 @@
         } else return false;
 
     }
-
+    
     if (!logged()) {
         
         header('Location: index.html');
 
     } else {
 
-        echo '
-            <nav class="navbar navbar--horizontal">
-                <ul class="navbar__list">
-                    <a class="navbar__item" href="scripts/php/logout.php">Wyloguj się</a>
-                    <a class="navbar__item" href="scripts/php/displayNotifications.php"><img src="/images/icons/notification_bell.png"</a>';
+        $url = $_SERVER['REQUEST_URI'];
 
-        if ($_SESSION['student'] != null) {
+        if ($url == '/subpages/studentPanel.php' || $url == '/subpages/teacherPanel.php') {
 
+                echo '
+                <nav class="navbar navbar--horizontal">
+                    <ul class="navbar__list">
+                        <a class="navbar__item" href="/scripts/php/logout.php">Wyloguj się</a>
+                        <a class="navbar__item" href="/scripts/php/snippets/displayNotifications.php"><img src="/images/icons/notification_bell.png"></a>
+                        <a class="navbar__item" href="/posts.php">Powrót do strony głównej</a>
+                    </ul>
+                </nav>';
+
+        } else {
+
+                echo '
+                <nav class="navbar navbar--horizontal">
+                    <ul class="navbar__list">
+                        <a class="navbar__item" href="/scripts/php/logout.php">Wyloguj się</a>
+                        <a class="navbar__item" href="/scripts/php/snippets/displayNotifications.php"><img src="/images/icons/notification_bell.png"</a>';
+
+            if ($_SESSION['student'] != null) {
+
+                echo '
+                        <a class="navbar__item" href="/subpages/studentPanel.php">Przejdź do panelu zarządzania</a>';
+
+            } elseif ($_SESSION['teacher'] != null) {
+
+                echo '
+                        <a class="navbar__item" href="/subpages/teacherPanel.php">Przejdź do panelu zarządzania</a>';
+
+            }
             echo '
-                    <a class="navbar__item" href="subpages/studentPanel.php">Przejdź do panelu zarządzania</a>';
-
-        } elseif ($_SESSION['teacher'] != null) {
-
-            echo '
-                    <a class="navbar__item" href="subpages/teacherPanel.php">Przejdź do panelu zarządzania</a>';
+                    </ul>
+                </nav>';
 
         }
-        echo '
-                </ul>
-            </nav>';
 
     }
 
