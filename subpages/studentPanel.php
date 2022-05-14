c57df4d421f2297d0d097611afda3e14f3bdd5c8
MarcelBienko3b <m.bienko.3b19@gmail.com>
2022-05-14 20:57:21 +0200

    Added 'Lato' font to every page's head

--- subpages/studentPanel.php
+++ subpages/studentPanel.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <title>Student panel</title>
     <link rel="stylesheet" href="../styles/pages/panel/panel.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body>
 <?php require '../scripts/php/snippets/nav.php' ?>
