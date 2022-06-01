15864694d42772a28872ff95fb9eac1f7e893909
megajajo <mateuszek.jareczek@gmail.com>
2022-06-01 14:57:35 +0200

    moved procedures

--- databases/zsti_together_struct.sql
+++ databases/zsti_together_struct.sql
@@ -111,3 +111,64 @@ select
         where posts.post__tutor=curr_Id;
 
 
+CREATE PROCEDURE notUserPosts
+(IN curr_Id int)
+select distinct
+    posts.post__body,
+    subjects.subject__name,
+    students.student__firstName,
+    students.student__lastName,
+    teachers.teacher__firstName,
+    teachers.teacher__lastName,
+    types.type__name,
+    classes.class__name,
+    posts.post__id
+from posts
+left join students on posts.post__tutor = students.student__id
+inner join subjects on subjects.subject__id = posts.post__subject
+left join teachers on teachers.teacher__id = posts.post__teacher
+inner join types on types.type__id = posts.post__type
+left join classes on classes.class__id = students.student__class
+    
+except
+
+select distinct
+    posts.post__body,
+    subjects.subject__name,
+    students.student__firstName,
+    students.student__lastName,
+    teachers.teacher__firstName,
+    teachers.teacher__lastName,
+    types.type__name,
+    classes.class__name,
+    posts.post__id
+from requests
+inner join posts on requests.request__post = posts.post__id
+left join students on posts.post__tutor = students.student__id
+inner join subjects on subjects.subject__id = posts.post__subject
+left join teachers on teachers.teacher__id = posts.post__teacher
+inner join types on types.type__id = posts.post__type
+left join classes on classes.class__id = students.student__class
+where requests.request__student=curr_Id;
+
+CREATE PROCEDURE userPosts
+(IN curr_Id int)
+select distinct
+    posts.post__body,
+    subjects.subject__name,
+    students.student__firstName,
+    students.student__lastName,
+    teachers.teacher__firstName,
+    teachers.teacher__lastName,
+    types.type__name,
+    classes.class__name,
+    posts.post__id
+from requests
+inner join posts on requests.request__post = posts.post__id
+left join students on posts.post__tutor = students.student__id
+inner join subjects on subjects.subject__id = posts.post__subject
+left join teachers on teachers.teacher__id = posts.post__teacher
+inner join types on types.type__id = posts.post__type
+left join classes on classes.class__id = students.student__class
+where requests.request__student=curr_Id;
+
